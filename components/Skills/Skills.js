c2b81a998610e5babcf75a7baccdb2a29f409b23
Kainoa Aqui <aquic@spu.edu>
2025-06-02 09:04:34 -0700

    update skills

--- components/Skills/Skills.js
+++ components/Skills/Skills.js
@@ -54,8 +54,7 @@ const Skills = () => {
               My Skills
             </h1>
             <h2 className="text-[1.65rem] font-medium md:max-w-lg w-full mt-2 staggered-reveal">
-              I like to take responsibility to craft aesthetic user experience
-              using modern frontend architecture.{" "}
+            Passionate about learning applied AI and driving innovation through practical solutions{" "}
             </h2>
           </div>
           <div className="mt-10">
